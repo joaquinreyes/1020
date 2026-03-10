ae20d7ff5fb35965a44ea4f059aa7e5fd945464f
Joaquin Reyes <32786414+joaquinreyes@users.noreply.github.com>
2026-03-10 16:07:01 -0300

    feat: load terms & conditions from backend API with fallback to locale

--- lib/components/terms_and_condition.dart
+++ lib/components/terms_and_condition.dart
@@ -1,6 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hop/managers/api_manager.dart' show kBaseURL, kClubID;
 import 'package:hop/utils/custom_extensions.dart';
 
 import '../app_styles/app_colors.dart';
@@ -9,16 +11,76 @@ import '../routes/app_pages.dart';
 import 'custom_dialog.dart';
 import 'main_button.dart';
 
-class TermsAndCondition extends ConsumerWidget {
+class TermsAndCondition extends ConsumerStatefulWidget {
   final bool showButton;
   final bool isTerms;
   final ScrollController scrollController;
 
   const TermsAndCondition(
-      {super.key, this.isTerms = true, this.showButton = false,required this.scrollController});
+      {super.key,
+      this.isTerms = true,
+      this.showButton = false,
+      required this.scrollController});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<TermsAndCondition> createState() => _TermsAndConditionState();
+}
+
+class _TermsAndConditionState extends ConsumerState<TermsAndCondition> {
+  String? _backendText;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPolicies();
+  }
+
+  Future<void> _fetchPolicies() async {
+    try {
+      final response = await Dio().get('$kBaseURL/$kClubID/policies');
+      final data = response.data?['data'];
+      if (data != null) {
+        final text = widget.isTerms
+            ? data['terms_and_conditions']
+            : data['privacy_policy'];
+        if (text != null && text.toString().isNotEmpty) {
+          // Strip HTML tags for plain text display
+          final stripped = text
+              .toString()
+              .replaceAll(RegExp(r'<[^>]*>'), '')
+              .replaceAll('&nbsp;', ' ')
+              .replaceAll('&amp;', '&')
+              .replaceAll('&lt;', '<')
+              .replaceAll('&gt;', '>')
+              .replaceAll('&quot;', '"')
+              .trim();
+          if (stripped.isNotEmpty) {
+            setState(() {
+              _backendText = stripped;
+            });
+          }
+        }
+      }
+    } catch (_) {
+      // Fallback to locale text
+    } finally {
+      if (mounted) {
+        setState(() {
+          _loading = false;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final fallbackText = widget.isTerms
+        ? "TERMS_AND_CONDITIONS_TEXT".tr(context)
+        : "COMMUNICATIONS_TEXT".tr(context);
+
+    final displayText = _backendText ?? fallbackText;
+
     return CustomDialog(
         color: AppColors.white,
         closeIconColor: AppColors.blue,
@@ -28,7 +90,7 @@ class TermsAndCondition extends ConsumerWidget {
           children: [
             Center(
                 child: Text(
-              isTerms
+              widget.isTerms
                   ? "TERMS_AND_CONDITIONS".trU(context)
                   : "COMMUNICATIONS".trU(context),
               style: AppTextStyles.popupHeaderTextStyle.copyWith(color: AppColors.blue),
@@ -37,16 +99,21 @@ class TermsAndCondition extends ConsumerWidget {
             SizedBox(
               height: 20.h,
             ),
-            Text(
-              isTerms
-                  ? "TERMS_AND_CONDITIONS_TEXT".tr(context)
-                  : "COMMUNICATIONS_TEXT".tr(context),
-              style: AppTextStyles.manropeMedium(
-                  fontSize: 15.sp, color: AppColors.blue,),
-              textAlign: TextAlign.center,
-            ),
+            _loading
+                ? Center(
+                    child: CircularProgressIndicator(
+                      color: AppColors.blue,
+                      strokeWidth: 2,
+                    ),
+                  )
+                : Text(
+                    displayText,
+                    style: AppTextStyles.manropeMedium(
+                        fontSize: 15.sp, color: AppColors.blue,),
+                    textAlign: TextAlign.center,
+                  ),
 
-            if (showButton)
+            if (widget.showButton)
               Padding(
                   padding: EdgeInsets.only(top: 20.h, left: 15, right: 15),
                   child: MainButton(
